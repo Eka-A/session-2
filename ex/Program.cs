01b82f9dbfe4543d203a01981dd1abf6dfe3cd50
Ekaterina Anikina <reityan97@mail.ru>
2022-07-16 17:37:31 +0300

    Решение первой задачи

---
+++ ex/Program.cs
@@ -0,0 +1,26 @@
+﻿void Ex_001()
+{
+    //Напишите программу, которая выводит третью цифру заданного числа 
+    //или сообщает, что третьей цифры нет.
+
+    Console.WriteLine ("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int i = Math.Abs (number);
+
+    while(i >= 999)
+    {
+        i /= 10;
+    }
+
+    int result = i;
+    string resultText = Convert.ToString(result);
+        if (resultText.Length > 2)
+        {
+        Console.WriteLine("третья цифра -> " + resultText[2]);
+        }
+        else 
+        {
+        Console.WriteLine("-> третьей цифры нет");
+        }
+}
+
